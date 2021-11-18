97787174ac32e6c18bcd7b053e82cc77d59f1f7c
Atul Chinchalkar <atulsc2010@gmail.com>
2021-11-18 15:34:44 +1100

    adjusted hardcoded wordlist size

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // Cryptographic replacement for Math.random()
 function randomNumberBetweenZeroAndOne() {
@@ -12,70 +12,69 @@ function generatePassword(numberOfWords) {
   // Empty array to be filled with wordlist
   var generatedPasswordArray = [];
 
-
   // Grab a random word, push it to the password array
   for (var i = 0; i < numberOfWords; i++) {
-      var index = Math.floor(randomNumberBetweenZeroAndOne() * 7776)
-      generatedPasswordArray.push(wordlist[index]);
+    var index = Math.floor(randomNumberBetweenZeroAndOne() * wordlist.length);
+    generatedPasswordArray.push(wordlist[index]);
   }
 
-  return generatedPasswordArray.join(' ');
+  return generatedPasswordArray.join(" ");
 }
 
 function setStyleFromWordNumber(passwordField, numberOfWords) {
-  var baseSize = '40';
-  var newSize = baseSize * (4/numberOfWords);
-  passwordField.setAttribute('style', 'font-size: ' + newSize + 'px;');
+  var baseSize = "40";
+  var newSize = baseSize * (4 / numberOfWords);
+  passwordField.setAttribute("style", "font-size: " + newSize + "px;");
 }
 
 function convertSecondsToReadable(seconds) {
-  var timeString = '';
-  var crackabilityColor = 'green';
+  var timeString = "";
+  var crackabilityColor = "green";
 
   // Enumerate all the numbers
   var numMilliseconds = seconds * 1000;
-  var numSeconds     = Math.floor(seconds);
-  var numMinutes     = Math.floor(numSeconds / 60);
-  var numHours       = Math.floor(numSeconds / (60 * 60));
-  var numDays        = Math.floor(numSeconds / (60 * 60 * 24));
-  var numYears       = Math.floor(numSeconds / (60 * 60 * 24 * 365));
-  var numCenturies   = Math.floor(numSeconds / (60 * 60 * 24 * 365 * 100));
+  var numSeconds = Math.floor(seconds);
+  var numMinutes = Math.floor(numSeconds / 60);
+  var numHours = Math.floor(numSeconds / (60 * 60));
+  var numDays = Math.floor(numSeconds / (60 * 60 * 24));
+  var numYears = Math.floor(numSeconds / (60 * 60 * 24 * 365));
+  var numCenturies = Math.floor(numSeconds / (60 * 60 * 24 * 365 * 100));
 
   if (numMilliseconds < 1000) {
-    timeString = numMilliseconds + ' milliseconds';
+    timeString = numMilliseconds + " milliseconds";
   } else if (numSeconds < 60) {
-    timeString = numSeconds + ' seconds';
+    timeString = numSeconds + " seconds";
   } else if (numMinutes < 60) {
-    timeString = numMinutes + ' minutes';
+    timeString = numMinutes + " minutes";
   } else if (numHours < 24) {
-    timeString = numHours + ' hours';
+    timeString = numHours + " hours";
   } else if (numDays < 365) {
-    timeString = numDays + ' days';
+    timeString = numDays + " days";
   } else if (numYears < 100) {
-    timeString = numYears + ' years';
+    timeString = numYears + " years";
   } else {
-    timeString = numCenturies + ' centuries';
+    timeString = numCenturies + " centuries";
   }
 
-  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function calculateAndSetCrackTime() {
   var timeToCrack = zxcvbn(passwordField.value);
   var readableCrackTime = convertSecondsToReadable(timeToCrack.crack_time);
-  document.querySelector('.crack-time').innerHTML = readableCrackTime;
+  document.querySelector(".crack-time").innerHTML = readableCrackTime;
 }
 
-var selectField = document.getElementById('passphrase_select');
-var passwordField = document.getElementById('passphrase');
-var button = document.querySelector('.btn-generate');
+var selectField = document.getElementById("passphrase_select");
+var passwordField = document.getElementById("passphrase");
+var button = document.querySelector(".btn-generate");
 
 // Initially run it upon load
-passwordField.setAttribute('value', generatePassword(4));
+passwordField.setAttribute("value", generatePassword(4));
 calculateAndSetCrackTime();
 
 // Listen for a button click
-button.addEventListener('click', function() {
+button.addEventListener("click", function () {
   var numberOfWords = selectField.options[selectField.selectedIndex].value;
   passwordField.value = generatePassword(numberOfWords);
   setStyleFromWordNumber(passwordField, numberOfWords);
@@ -83,6 +82,6 @@ button.addEventListener('click', function() {
 });
 
 // Listen for password value change
-passwordField.addEventListener('input', function (evt) {
+passwordField.addEventListener("input", function (evt) {
   calculateAndSetCrackTime();
 });
