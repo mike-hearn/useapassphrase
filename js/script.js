89d7cd41853513e6fb5c2ff6cdd3fe688ed73377
Atul Chinchalkar <atulsc2010@gmail.com>
2021-11-18 16:12:52 +1100

    removed single to double quotes autoformatting

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Cryptographic replacement for Math.random()
 function randomNumberBetweenZeroAndOne() {
@@ -18,18 +18,18 @@ function generatePassword(numberOfWords) {
     generatedPasswordArray.push(wordlist[index]);
   }
 
-  return generatedPasswordArray.join(" ");
+  return generatedPasswordArray.join(' ');
 }
 
 function setStyleFromWordNumber(passwordField, numberOfWords) {
-  var baseSize = "40";
+  var baseSize = '40';
   var newSize = baseSize * (4 / numberOfWords);
-  passwordField.setAttribute("style", "font-size: " + newSize + "px;");
+  passwordField.setAttribute('style', 'font-size: ' + newSize + 'px;');
 }
 
 function convertSecondsToReadable(seconds) {
-  var timeString = "";
-  var crackabilityColor = "green";
+  var timeString = '';
+  var crackabilityColor = 'green';
 
   // Enumerate all the numbers
   var numMilliseconds = seconds * 1000;
@@ -41,40 +41,40 @@ function convertSecondsToReadable(seconds) {
   var numCenturies = Math.floor(numSeconds / (60 * 60 * 24 * 365 * 100));
 
   if (numMilliseconds < 1000) {
-    timeString = numMilliseconds + " milliseconds";
+    timeString = numMilliseconds + ' milliseconds';
   } else if (numSeconds < 60) {
-    timeString = numSeconds + " seconds";
+    timeString = numSeconds + ' seconds';
   } else if (numMinutes < 60) {
-    timeString = numMinutes + " minutes";
+    timeString = numMinutes + ' minutes';
   } else if (numHours < 24) {
-    timeString = numHours + " hours";
+    timeString = numHours + ' hours';
   } else if (numDays < 365) {
-    timeString = numDays + " days";
+    timeString = numDays + ' days';
   } else if (numYears < 100) {
-    timeString = numYears + " years";
+    timeString = numYears + ' years';
   } else {
-    timeString = numCenturies + " centuries";
+    timeString = numCenturies + ' centuries';
   }
 
-  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return timeString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function calculateAndSetCrackTime() {
   var timeToCrack = zxcvbn(passwordField.value);
   var readableCrackTime = convertSecondsToReadable(timeToCrack.crack_time);
-  document.querySelector(".crack-time").innerHTML = readableCrackTime;
+  document.querySelector('.crack-time').innerHTML = readableCrackTime;
 }
 
-var selectField = document.getElementById("passphrase_select");
-var passwordField = document.getElementById("passphrase");
-var button = document.querySelector(".btn-generate");
+var selectField = document.getElementById('passphrase_select');
+var passwordField = document.getElementById('passphrase');
+var button = document.querySelector('.btn-generate');
 
 // Initially run it upon load
-passwordField.setAttribute("value", generatePassword(4));
+passwordField.setAttribute('value', generatePassword(4));
 calculateAndSetCrackTime();
 
 // Listen for a button click
-button.addEventListener("click", function () {
+button.addEventListener('click', function () {
   var numberOfWords = selectField.options[selectField.selectedIndex].value;
   passwordField.value = generatePassword(numberOfWords);
   setStyleFromWordNumber(passwordField, numberOfWords);
@@ -82,6 +82,6 @@ button.addEventListener("click", function () {
 });
 
 // Listen for password value change
-passwordField.addEventListener("input", function (evt) {
+passwordField.addEventListener('input', function (evt) {
   calculateAndSetCrackTime();
 });
